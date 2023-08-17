8d22c9abc35c53c5e1435f274c5297eb5ecba3c9
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-08-17 13:16:28 +1200

    Implemented global styles

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from "./App";
+import { createGlobalStyle } from 'styled-components';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const GlobalStyle = createGlobalStyle`
+    body {
+        font-family: ${(props) => props.$fontFamily || "Helvetica Neue"};
+    }
+`
+ReactDOM.render(
+    <React.Fragment>
+      <GlobalStyle $fontFamily="Fredoka One" /> 
+      <App />
+    </React.Fragment>,
+    document.getElementById("root")
+  );
